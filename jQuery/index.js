8b47ee63215f5528a41d130167ea2330df4ca4f3
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-08-21 09:40:03 +0545

    Using On like for click , keypress.

--- jQuery/index.js
+++ jQuery/index.js
@@ -4,3 +4,7 @@ $("h1").addClass("big-text margin");
 
 $(document).keypress(function(event) {
     $("h1").html(event.key);})
+
+$(document).on('click',function(){
+    $("h1").css("color", "purple");
+});
