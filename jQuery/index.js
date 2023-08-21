019e0a01c2984e2e15747977eea027025b649615
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-08-21 19:00:09 +0545

    Added Animation by using jQuery.

--- jQuery/index.js
+++ jQuery/index.js
@@ -9,8 +9,27 @@ $(document).on('click',function(){
     $("h1").css("color", "purple");
 });
 
+//Adding and Removing Elements
 $("h1").before("<button>Click</button>");
 $("h1").after("<button>Click</button>");
 $("h1").prepend("<button>Click</button>");
 $("h1").append("<button>Click</button>");
 button.remove();
+
+
+//Adding animations
+
+$("h1").hide();
+$("h1").show();
+$("h1").toggle(); //both hide and show
+
+$("h1").fadeIn();
+$("h1").fadeOut();
+$("h1").fadeToggle();//both fadeIn and fadeOut
+
+//We can also chain it
+$("h1").slideUp().slideDown().animate({"opacity:0.5"});
+
+//For Custom CSS
+$("h1").animate({"opacity:0.5"});
+$("h1").animate({"margin:20px"});
